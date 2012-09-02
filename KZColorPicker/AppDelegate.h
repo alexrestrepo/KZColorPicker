d71b5edb575b30874fcea9fee102e58c62b3c381
Alex Restrepo <alexrestrepo@mac.com>
2012-09-01 18:10:40 -0700

    Initial Commit

---
+++ KZColorPicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KZColorPicker
+//
+//  Created by Alex Restrepo on 9/1/12.
+//  Copyright (c) 2012 Alex Restrepo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KZColorPicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KZColorPicker
+//
+//  Created by Alex Restrepo on 9/1/12.
+//  Copyright (c) 2012 Alex Restrepo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
